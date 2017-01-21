45d7114f2ca8abe97bafd8db7c224b4f025f3d1d
Michael Chau <Michael Chau>
2017-01-21 14:28:39 -0500

    Initial scorekeeping mechanism

---
+++ blindsSim/Assets/scripts/scoreKeeping.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreKeeping : MonoBehaviour {
+    private int score =0 ;
+    public GameObject star;
+    public GameObject star1;
+    public GameObject star2;
+    Vector3 start1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        score++;
+        print(score);
+        star.GetComponent<Rigidbody2D>().velocity = new Vector3(20, 0, 0);
+        star1.GetComponent<Rigidbody2D>().velocity = new Vector3(20, 0, 0);
+        star2.GetComponent<Rigidbody2D>().velocity = new Vector3(20, 0, 0);
+    }
+}
